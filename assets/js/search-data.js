a32f2a5cd25cd8976a665fbf0e570414bcc00431
spookyvoiddev <42708672+spookyvoiddev@users.noreply.github.com>
2026-05-15 14:10:09 +0000

    Deploying to gh-pages from @ spookyvoiddev/spookyvoiddev.github.io@7d96fa207508e1e3a605e7f317e2faf141f2d45c 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,329 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "Tabletop and digital games. Designer, narrative, audio, and creative-lead work.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "Work experience, education, and selected independent projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "post-a-year-in-review",
+        
+          title: "A Year In Review",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/a-year-in-review/";
+          
+        },
+      },{id: "post-roll20-marketplace-aspirations",
+        
+          title: "Roll20 Marketplace Aspirations",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2022/roll20-marketplace-aspirations/";
+          
+        },
+      },{id: "post-wanderer-s-guide-to-prosgia",
+        
+          title: "Wanderer’s Guide to Prosgia",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2022/wanderers-guide-to-prosgia/";
+          
+        },
+      },{id: "post-written-in-ink",
+        
+          title: "Written In Ink",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/written-in-ink/";
+          
+        },
+      },{id: "post-summer-prospects",
+        
+          title: "Summer Prospects",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/summer-prospects/";
+          
+        },
+      },{id: "post-acolyte-postmortem",
+        
+          title: "Acolyte Postmortem",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/acolyte-postmortem/";
+          
+        },
+      },{id: "post-acolyte",
+        
+          title: "Acolyte",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/acolyte/";
+          
+        },
+      },{id: "post-battlemap-iteration",
+        
+          title: "Battlemap Iteration",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2021/battlemap-iteration/";
+          
+        },
+      },{id: "post-naughty-or-nice",
+        
+          title: "Naughty Or Nice",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2020/naughty-or-nice/";
+          
+        },
+      },{id: "post-godot-and-game-development",
+        
+          title: "Godot and Game Development",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2020/godot-and-game-development/";
+          
+        },
+      },{id: "post-dungeondraft-mapmaking-and-adventure-design",
+        
+          title: "Dungeondraft, Mapmaking, and Adventure Design",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2020/dungeondraft-mapmaking-and-adventure-design/";
+          
+        },
+      },{id: "post-jojo-the-flying-cat-mod",
+        
+          title: "Jojo, The Flying Cat Mod",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2020/jojo-the-flying-cat-mod/";
+          
+        },
+      },{id: "post-tabletop-simulator-and-playtesting",
+        
+          title: "Tabletop Simulator and Playtesting",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2020/tabletop-simulator-and-playtesting/";
+          
+        },
+      },{id: "post-machinations-and-game-design-tools",
+        
+          title: "Machinations and Game Design Tools",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2020/machinations-and-game-design-tools/";
+          
+        },
+      },{id: "post-marianne-s-paralogue-and-character-expectations",
+        
+          title: "Marianne’s Paralogue and Character Expectations",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2019/mariannes-paralogue-and-character-expectations/";
+          
+        },
+      },{id: "post-first-ever-encounter",
+        
+          title: "First Ever Encounter!",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2019/first-ever-encounter/";
+          
+        },
+      },{id: "post-battle-brothers-the-tale-of-sun-s-end-company",
+        
+          title: "Battle Brothers – The Tale of Sun’s End Company",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2019/battle-brothers-the-tale-of-suns-end-company/";
+          
+        },
+      },{id: "post-mount-amp-blade-the-ultimate-medieval-low-fantasy-sandbox",
+        
+          title: "Mount &amp; Blade: The Ultimate Medieval/Low-Fantasy Sandbox",
+        
+        description: "Migrated from youcanclickl3.wordpress.com",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2019/mount-blade-is-there-any-better-way-to-start-a-game-design-blog/";
+          
+        },
+      },{id: "projects-acolyte",
+          title: 'Acolyte',
+          description: "Indie TTRPG about apprentice wizards on their graduation quest.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/acolyte/";
+            },},{id: "projects-lost-in-translation",
+          title: 'Lost in Translation',
+          description: "Short choose-your-own-adventure game. Narrative and sound designer.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/lost-in-translation/";
+            },},{id: "projects-plane-disappointment",
+          title: 'Plane Disappointment',
+          description: "Endless runner. Programmer and sound designer.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/plane-disappointment/";
+            },},{id: "projects-seventino-midnight-cafe",
+          title: 'Seventino Midnight Cafe',
+          description: "Cooking game with hunting mechanic. Narrative designer.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/seventino-midnight-cafe/";
+            },},{id: "projects-ttrpg-battlemaps",
+          title: 'TTRPG Battlemaps',
+          description: "16-map gallery of grid-ready battlemaps for tabletop role-playing.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ttrpg-battlemaps/";
+            },},{id: "projects-wanderer-39-s-guide-to-prosgia",
+          title: 'Wanderer&amp;#39;s Guide to Prosgia',
+          description: "5th-edition TTRPG supplement. Creative lead on a 10-person team.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/wanderers-guide-to-prosgia/";
+            },},{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+        },
+      },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-inspire',
+        title: 'Inspire HEP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
