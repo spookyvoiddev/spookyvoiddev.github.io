74198839b82ce5b022b7e7949d30170a64580e19
spookyvoiddev <42708672+spookyvoiddev@users.noreply.github.com>
2026-05-15 14:37:46 +0000

    Deploying to gh-pages from @ spookyvoiddev/spookyvoiddev.github.io@b693e1b022f10459ad3be2c506abb8106152e411 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "Tabletop and digital games. Designer, narrative, audio, and creative-lead work.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "nav-cv",
           title: "cv",
